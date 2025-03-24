9287b36ee2717606fbbf33082b486414ba7dfc5e
Pedro Lucas <pedro.lucaswpl@gmail.com>
2025-03-24 15:52:26 -0300

    refactor: using env vars

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// server.js
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
@@ -8,28 +8,36 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const TELEGRAM_BOT_TOKEN = "BOT-TOKEN"; // Substitua pelo token do seu bot
-const TELEGRAM_CHAT_ID = "CHAT-TOKEN"; // Substitua pelo ID do chat (ou grupo) para onde quer enviar
+const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID;
+const PORT = process.env.PORT || 8088;
+
+if (!TELEGRAM_BOT_TOKEN || !TELEGRAM_CHAT_ID) {
+  console.error("Erro: As variáveis de ambiente TELEGRAM_BOT_TOKEN e TELEGRAM_CHAT_ID devem ser definidas.");
+  process.exit(1);
+}
 
 app.post("/send-location", async (req, res) => {
   const { latitude, longitude } = req.body;
 
+  if (!latitude || !longitude) {
+    return res.status(400).json({ success: false, message: "Latitude e longitude são obrigatórios." });
+  }
+
   const message = `A localização do usuário é:\nLatitude: ${latitude}\nLongitude: ${longitude}`;
 
   try {
-    // Envia a localização para o Telegram
     await axios.post(`https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`, {
       chat_id: TELEGRAM_CHAT_ID,
       text: message,
     });
-
     res.status(200).json({ success: true });
   } catch (error) {
-    console.log(error);
+    console.error("Erro ao enviar a localização para o Telegram:", error);
     res.status(500).json({ success: false, message: "Erro ao enviar a localização para o Telegram." });
   }
 });
 
-app.listen(8088, () => {
-  console.log("Servidor rodando na porta 8088");
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
